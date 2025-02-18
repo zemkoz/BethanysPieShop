d9f383e5d95ad50f1cdf649f78777f6b9ae224cd
Zoltan Zemko <zoltan.zemko@hartmann.info>
2025-02-18 14:21:51 +0100

    Add Employee Overview page

---
+++ BethanysPieShop/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShop.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShop.Components.Pages;
+
+public partial class EmployeeOverview : ComponentBase
+{
+    private IList<Employee> Employees { get; set; } = default!;
+    
+    protected override void OnInitialized()
+    {
+        Employees = MockDataService.Employees;
+    }
+}
