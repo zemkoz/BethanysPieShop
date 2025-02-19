d488853a716392fc018b1e0fa34d933403be13ba
Zoltan Zemko <zoltan.zemko@hartmann.info>
2025-02-19 14:35:11 +0100

    Replace the employees' table with EmployeeCard component on the employee overview page.

---
+++ BethanysPieShop/Components/EmployeeCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShop.Components;
+
+public partial class EmployeeCard : ComponentBase
+{
+    [Parameter]
+    public Employee Employee { get; set; } = default!;
+}
